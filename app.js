d936a61eef41f07342180b5c28032b1b952c1cba
Denys Volodko <babyleejane@gmail.com>
2024-01-15 14:30:08 +0200

    basic websocket connection

---
+++ app.js
@@ -0,0 +1,17 @@
+const ws = new require('ws');
+
+const wsServer = new ws.Server({ port: 5000 });
+
+const socketList = [];
+
+wsServer.on('connection', (socket) => {
+  socketList.push(socket);
+  setTimeout(() => {
+    socket.send('Welcome to web-socket server');
+  }, 3000);
+  socketList.forEach((item) => {
+    if (item !== socket) {
+      item.send('New member connect');
+    }
+  });
+});
